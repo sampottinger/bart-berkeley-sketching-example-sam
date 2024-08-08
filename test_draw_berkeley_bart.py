9efd33e4ae7b84d5649c98e254eaf208c5715a72
A Samuel Pottinger <sam@gleap.org>
2024-08-08 16:16:29 +0000

    Fix style issues.

--- test_draw_berkeley_bart.py
+++ test_draw_berkeley_bart.py
@@ -25,15 +25,15 @@ class BerkeleyBartTests(unittest.TestCase):
         self.assertEqual(result.get_name(), 'test')
         self.assertEqual(result.get_code(), 'te')
         self.assertEqual(result.get_count(), 1234)
-    
+
     def test_get_line_length_zero(self):
         presenter = draw_berkeley_bart.StationVizPresenter(self._sketch)
         self.assertEqual(presenter._get_line_length(100, 0), draw_berkeley_bart.LINE_MIN_LEN)
-    
+
     def test_get_line_length_max(self):
         presenter = draw_berkeley_bart.StationVizPresenter(self._sketch)
         self.assertEqual(presenter._get_line_length(100, 100), draw_berkeley_bart.LINE_MAX_LEN)
-    
+
     def test_get_line_length_half(self):
         presenter = draw_berkeley_bart.StationVizPresenter(self._sketch)
         halfway = (draw_berkeley_bart.LINE_MAX_LEN + draw_berkeley_bart.LINE_MIN_LEN) / 2
